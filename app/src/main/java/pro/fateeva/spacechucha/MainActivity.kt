f5711d78c7d42658bd90a3336b8f508b914c6ea3
Margarita Fateeva <margarita@fateev.pro>
2022-01-04 23:34:21 +0300

    Fragments scroll

--- app/src/main/java/pro/fateeva/spacechucha/MainActivity.kt
+++ app/src/main/java/pro/fateeva/spacechucha/MainActivity.kt
@@ -2,20 +2,32 @@ package pro.fateeva.spacechucha
 
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
 import pro.fateeva.spacechucha.databinding.ActivityMainBinding
-import pro.fateeva.spacechucha.databinding.PictureOfTheDayFragmentBinding
 import pro.fateeva.spacechucha.view.*
 import pro.fateeva.spacechucha.view.ThemesDialogFragment.Companion.MARS
 import pro.fateeva.spacechucha.view.ThemesDialogFragment.Companion.MOON
 import pro.fateeva.spacechucha.view.ThemesDialogFragment.Companion.THEME
 import pro.fateeva.spacechucha.view.ThemesDialogFragment.Companion.THEME_NAME
 
+private const val NUM_PAGES = 5
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mPager: ViewPager
+
+    private var fragmentsList = listOf(
+        PictureOfTheDayFragment(),
+        EarthFragment(),
+        MarsFragment(),
+        MoonFragment(),
+        SettingsFragment()
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,47 +45,60 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(view)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, PictureOfTheDayFragment.newInstance()).commit()
+        mPager = binding.pager
+        val pagerAdapter = PagerAdapter(supportFragmentManager, fragmentsList)
+        mPager.adapter = pagerAdapter
 
         initBottomNavigation()
     }
 
+    override fun onBackPressed() {
+        if (mPager.currentItem == 0) {
+            super.onBackPressed()
+        } else {
+            mPager.currentItem = mPager.currentItem - 1
+        }
+    }
+
     fun initBottomNavigation() {
-        bottomNavigation.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.pictureOfTheDay -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.container, PictureOfTheDayFragment.newInstance())
-                        .commit()
-                    true
+        mPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) = Unit
+
+            override fun onPageSelected(position: Int) {
+                binding.bottomNavigation.selectedItemId = when(position) {
+                    0 -> R.id.pictureOfTheDay
+                    1 -> R.id.earth
+                    2 -> R.id.mars
+                    3 -> R.id.moon
+                    4 -> R.id.settings
+                    else -> R.id.pictureOfTheDay
                 }
-                R.id.earth -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.container, EarthFragment.newInstance())
-                        .commit()
-                    true
-                }
-                R.id.mars -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.container, MarsFragment.newInstance())
-                        .commit()
-                    true
-                }
-                R.id.moon -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.container, MoonFragment.newInstance())
-                        .commit()
-                    true
-                }
-                R.id.settings -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.container, SettingsFragment.newInstance())
-                        .commit()
-                    true
-                }
-                else -> false
             }
+
+            override fun onPageScrollStateChanged(state: Int)= Unit
+        })
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            mPager.currentItem = when (item.itemId) {
+                R.id.pictureOfTheDay -> 0
+                R.id.earth -> 1
+                R.id.mars -> 2
+                R.id.moon -> 3
+                R.id.settings -> 4
+                else -> 0
+            }
+            true
+        }
+    }
+
+    private inner class PagerAdapter(fm: FragmentManager, fragmentsList: List<Fragment>) :
+        FragmentStatePagerAdapter(fm) {
+        override fun getCount(): Int = NUM_PAGES
+        override fun getItem(position: Int): Fragment {
+            return fragmentsList[position]
         }
     }
 }
